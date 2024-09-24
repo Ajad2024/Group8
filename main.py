05364e4ec2f5f77997c7b444f21728521b4f6b74
Ajad2024 <s230066@dtu.dk>
2024-09-24 19:26:40 +0200

    o

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
+import sys
 import ifcopenshell
+sys.path.append('path_to_rules_directory')
 
-from .rules import windowRule
-from .rules import doorRule
-
-
+from rules import windowRule
+from rules import doorRule
+from rules import doorType
+from rules import numberofDoor
 
 model = ifcopenshell.open("D:/BIM/CES_BLD_24_06_ARC.ifc")
 
 windowResult = windowRule.checkRule(model)
 doorResult = doorRule.checkRule(model)
+doorTypeResult= doorType.checkRule(model)
 
 print("Window result:", windowResult)
+print("Door result:", doorResult)
+print("Door Type result:", doorTypeResult)
 print("Door result:", doorResult)
