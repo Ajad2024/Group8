68b2d33a27dd598161e1c4728917a01fbfa5ae04
Ajad2024 <s230066@dtu.dk>
2024-10-29 00:14:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from rules import windowRule
 from rules import doorRule
 from rules import doorType
 from rules import numberofDoor
-from rules import Furniturerole
 
 model = ifcopenshell.open("D:\BIM\CES_BLD_24_10_ARC.ifc")
 
@@ -14,10 +13,8 @@ windowResult = windowRule.checkRule(model)
 doorResult = doorRule.checkRule(model)
 doorTypeResult= doorType.checkRule(model)
 numberofDoorResult = numberofDoor.checkRule(model)
-numberofFurnitureResult = Furniturerole.checkRule(model)
 
 print("Window result:", windowResult)
 print("Door result:", doorResult)
 print("Door Type result:", doorTypeResult)
 print("Door result:", numberofDoorResult)
-print("Furniture result:", numberofFurnitureResult)
