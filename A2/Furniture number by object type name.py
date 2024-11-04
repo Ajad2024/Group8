fe742b6ea3a6abb70aa81468d007673d0b2de516
Ajad2024 <s230066@dtu.dk>
2024-11-04 23:49:56 +0100

    Update Furniture number by object type name.py

--- A2/Furniture number by object type name.py
+++ A2/Furniture number by object type name.py
@@ -1,1 +1,24 @@
 
+import ifcopenshell
+
+
+ifc_file_path = "D:\\BIM\\CES_BLD_24_10_ARC.ifc"  
+
+ifc_file = ifcopenshell.open(ifc_file_path)
+
+
+furniture_items = ifc_file.by_type("IfcFurniture")
+
+
+target_object_type = "Schreibtisch-Kombination-02 mit PC:Typ1"
+
+
+specific_item_count = 0
+
+
+for furniture in furniture_items:
+    if getattr(furniture, 'ObjectType', '') == target_object_type:
+        specific_item_count += 1
+
+
+print(f"Total number of '{target_object_type}' items: {specific_item_count}")
