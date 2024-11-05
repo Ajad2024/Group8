36d9b5672748052220e67cd4217770f0237ffd05
Ajad2024 <s230066@dtu.dk>
2024-11-05 10:12:52 +0100

    Update Blender slab area.py

--- A2/Blender slab area.py
+++ A2/Blender slab area.py
@@ -1,1 +1,12 @@
+import bpy
 
+total_area = 0.0
+
+for obj in bpy.context.scene.objects:
+    if "IfcSlab" in obj.name:
+        if obj.type == 'MESH':
+            slab_area = sum([face.area for face in obj.data.polygons])
+            total_area += slab_area
+            print(f"Area of IfcSlab '{obj.name}': {slab_area:.2f} square meters")
+
+print("Total area of all IfcSlab elements:", total_area, "square meters")
